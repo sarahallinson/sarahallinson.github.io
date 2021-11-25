1d3473f1b643d90d86bad94e2f202e0a4a66894d
sarahallinson <sall29@student.monash.edu>
2021-11-25 16:57:55 +1100

    Folio

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("pageprog").style.width = scrolled + "%";
+}
+
+// credit: https://www.w3schools.com/howto/howto_js_scroll_indicator.asp
